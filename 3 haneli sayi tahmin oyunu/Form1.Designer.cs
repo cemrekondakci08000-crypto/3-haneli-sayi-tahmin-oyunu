c0e2ec7e770f0c22255d13060ba6b8b8e81be7d3
CEMRE <CEMRE@LAPTOP-ITN2HD33>
2025-10-31 11:34:37 +0300

    Proje dosyası ekle.

---
+++ 3 haneli sayi tahmin oyunu/Form1.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace _3_haneli_sayi_tahmin_oyunu
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.lblSonuc = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblYazi = new System.Windows.Forms.Label();
+            this.btnTahmin = new System.Windows.Forms.Button();
+            this.txtTahmin = new System.Windows.Forms.TextBox();
+            this.lblKalanHak = new System.Windows.Forms.Label();
+            this.btnBasla = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Pink;
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Controls.Add(this.listBox1);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.lblYazi);
+            this.panel1.Controls.Add(this.btnTahmin);
+            this.panel1.Controls.Add(this.txtTahmin);
+            this.panel1.Controls.Add(this.lblKalanHak);
+            this.panel1.Controls.Add(this.btnBasla);
+            this.panel1.ForeColor = System.Drawing.Color.White;
+            this.panel1.Location = new System.Drawing.Point(27, 26);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(459, 417);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.LavenderBlush;
+            this.panel2.Controls.Add(this.lblSonuc);
+            this.panel2.Location = new System.Drawing.Point(91, 145);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(243, 40);
+            this.panel2.TabIndex = 1;
+            // 
+            // lblSonuc
+            // 
+            this.lblSonuc.AutoSize = true;
+            this.lblSonuc.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblSonuc.ForeColor = System.Drawing.Color.SlateBlue;
+            this.lblSonuc.Location = new System.Drawing.Point(19, 13);
+            this.lblSonuc.Name = "lblSonuc";
+            this.lblSonuc.Size = new System.Drawing.Size(46, 18);
+            this.lblSonuc.TabIndex = 5;
+            this.lblSonuc.Text = "label2";
+            // 
+            // listBox1
+            // 
+            this.listBox1.BackColor = System.Drawing.Color.LavenderBlush;
+            this.listBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.listBox1.ForeColor = System.Drawing.Color.SlateBlue;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 18;
+            this.listBox1.Location = new System.Drawing.Point(80, 253);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(254, 144);
+            this.listBox1.TabIndex = 8;
+            this.listBox1.Tag = "";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.ForeColor = System.Drawing.Color.SlateBlue;
+            this.label4.Location = new System.Drawing.Point(77, 225);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(120, 18);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Tahmin Geçmişi:";
+            // 
+            // lblYazi
+            // 
+            this.lblYazi.AutoSize = true;
+            this.lblYazi.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblYazi.ForeColor = System.Drawing.Color.SlateBlue;
+            this.lblYazi.Location = new System.Drawing.Point(125, 197);
+            this.lblYazi.Name = "lblYazi";
+            this.lblYazi.Size = new System.Drawing.Size(46, 18);
+            this.lblYazi.TabIndex = 6;
+            this.lblYazi.Text = "label3";
+            // 
+            // btnTahmin
+            // 
+            this.btnTahmin.BackColor = System.Drawing.Color.HotPink;
+            this.btnTahmin.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnTahmin.Location = new System.Drawing.Point(216, 95);
+            this.btnTahmin.Name = "btnTahmin";
+            this.btnTahmin.Size = new System.Drawing.Size(118, 31);
+            this.btnTahmin.TabIndex = 4;
+            this.btnTahmin.Text = "Tahmin Et";
+            this.btnTahmin.UseVisualStyleBackColor = false;
+            this.btnTahmin.Click += new System.EventHandler(this.btnTahmin_Click);
+            // 
+            // txtTahmin
+            // 
+            this.txtTahmin.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtTahmin.ForeColor = System.Drawing.Color.SlateBlue;
+            this.txtTahmin.Location = new System.Drawing.Point(80, 100);
+            this.txtTahmin.Name = "txtTahmin";
+            this.txtTahmin.Size = new System.Drawing.Size(100, 24);
+            this.txtTahmin.TabIndex = 3;
+            this.txtTahmin.Text = "Tahmin (3 haneli bir sayı giriniz)";
+            // 
+            // lblKalanHak
+            // 
+            this.lblKalanHak.AutoSize = true;
+            this.lblKalanHak.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblKalanHak.ForeColor = System.Drawing.Color.SlateBlue;
+            this.lblKalanHak.Location = new System.Drawing.Point(170, 65);
+            this.lblKalanHak.Name = "lblKalanHak";
+            this.lblKalanHak.Size = new System.Drawing.Size(46, 18);
+            this.lblKalanHak.TabIndex = 2;
+            this.lblKalanHak.Text = "label1";
+            // 
+            // btnBasla
+            // 
+            this.btnBasla.BackColor = System.Drawing.Color.HotPink;
+            this.btnBasla.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnBasla.ForeColor = System.Drawing.Color.White;
+            this.btnBasla.Location = new System.Drawing.Point(80, 17);
+            this.btnBasla.Name = "btnBasla";
+            this.btnBasla.Size = new System.Drawing.Size(254, 35);
+            this.btnBasla.TabIndex = 1;
+            this.btnBasla.Text = "Başlat/Yeniden Başlat";
+            this.btnBasla.UseVisualStyleBackColor = false;
+            this.btnBasla.Click += new System.EventHandler(this.btnBasla_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(577, 477);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = " ";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblYazi;
+        private System.Windows.Forms.Label lblSonuc;
+        private System.Windows.Forms.Button btnTahmin;
+        private System.Windows.Forms.TextBox txtTahmin;
+        private System.Windows.Forms.Label lblKalanHak;
+        private System.Windows.Forms.Button btnBasla;
+        private System.Windows.Forms.Panel panel2;
+    }
+}
+
---
+++ 3 haneli sayi tahmin oyunu/Form1.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _3_haneli_sayi_tahmin_oyunu
+{
+    public partial class Form1 : Form
+    {
+        int tahmin_sayisi = 10; string dogrusayi;
+        public Form1()
+        {
+            InitializeComponent();
+            dogrusayi = rastgelesayiüret();
+            this.AcceptButton = btnTahmin;
+        }
+        private string rastgelesayiüret()
+        {
+            Random rnd = new Random();
+            int sayi;
+            int birler, onlar, yuzler;
+            bool rakamlarfarkli;
+
+            do
+            {
+                sayi=rnd.Next(100,1000);
+                birler = sayi % 10;
+                onlar = (sayi / 10)%10;
+                yuzler = sayi / 100;
+                if(birler!=onlar && birler!=yuzler && onlar!=yuzler)
+                {
+                    rakamlarfarkli = true;
+                }
+                else
+                {
+                    rakamlarfarkli = false;
+                }
+
+            }while(rakamlarfarkli==false);
+            return dogrusayi=sayi.ToString();
+        }
+       
+        private void btnTahmin_Click(object sender, EventArgs e)
+        {
+            string tahmin=txtTahmin.Text;
+            txtTahmin.Clear();
+            listBox1.Visible = true;
+            label4.Visible = true;
+            panel2.Visible = true;
+            
+            if (tahmin.Length != 3)
+            {
+                lblSonuc.Text = "lütfen 3 basamakli bir sayi giriniz";
+                lblYazi.Text = "girdiginiz sayı 3 haneli olmalı";
+                return;
+            }
+            if (tahmin.StartsWith("0"))
+            {
+                lblSonuc.Text = "sayı sıfır ile başlayamaz";
+                lblYazi.Text = "ilk rakam 1-9 arasında olmalı";
+                return;
+            }
+            if(tahmin.Length!=tahmin.Distinct().Count())
+            {
+                lblSonuc.Text = "rakamlar birbirinden farkli olmali";
+                lblYazi.Text = "örnek=123,789 gibi (tekrar eden rakam yok)";
+                return;
+            }
+            if (tahmin_sayisi > 0)
+            {
+
+                tahmin_sayisi--;
+                lblKalanHak.Text = "kalan hak:" + tahmin_sayisi.ToString();
+            }
+            else
+            {
+                lblSonuc.Text = "oyun bitti! dogru sayı:" + dogrusayi;
+                lblYazi.Text = "yeniden başlamak için başlat butonuna tıklayın";
+                return;
+            }
+            if(tahmin==dogrusayi)
+            {
+                lblSonuc.Text = "dogru sayıyı buldunuz..:)";
+            }
+            int dogruyer = 0;
+            int yanlisyer = 0;
+
+            bool[] dogrukullanildi = new bool[3];
+            bool[] tahminkullanildi = new bool[3];
+
+            for (int i = 0; i < 3; i++)
+            {
+                if (tahmin[i] == dogrusayi[i])
+                {
+                    dogruyer++;
+                    dogrukullanildi[i] = true;
+                    tahminkullanildi[i] = true;
+                }
+            }
+            for (int i = 0; i < 3; i++)
+            {
+                if (tahminkullanildi[i]) continue;
+                for (int j = 0; j < 3; j++)
+                {
+                    if (dogrukullanildi[j]) continue;
+                    if (tahmin[i] == dogrusayi[j])
+                    {
+                        yanlisyer++;
+                        dogrukullanildi[j] = true;
+                        break;
+                    }
+                }
+
+            }
+
+            if(dogruyer == 0 && yanlisyer == 0)
+            {
+                lblSonuc.Visible=true;
+                lblYazi.Text = "hiç bir rakam dogru degil";
+            }
+            if(dogruyer>0 && yanlisyer > 0)
+            {
+                lblSonuc.Visible=true ;
+                lblYazi.Text =dogruyer+ "rakamin yeri dogru,"+yanlisyer+"rakam dogru ancak yeri yanlış";
+            }
+            else if(dogruyer>0 )
+            {
+                lblSonuc.Visible=true ;
+                lblYazi.Text = dogruyer + "rakamin yeri dogru";
+            }
+            else if(yanlisyer>0 )
+            {
+                lblSonuc.Visible=true ;
+                lblYazi.Text = yanlisyer + "rakam dogru ama yeri yanlış";
+            }
+
+
+
+            string sonuc = $"+{dogruyer}-{yanlisyer}";
+            //lblYazi.Text = $"+{dogruyer} rakam dogru yerde,{yanlisyer} rakam dogru ama yeri yanlış";
+            lblSonuc.Text = $"sonuç:{sonuc}";
+            listBox1.Items.Add($"tahmin{tahmin}->{sonuc}");
+
+
+
+
+
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            txtTahmin.Enabled = false;
+            lblSonuc.Text = "";
+            lblYazi.Text = "";
+            lblKalanHak.Text = "";
+            listBox1.Visible = false;
+            label4.Visible = false;
+            panel2.Visible = false;
+          
+        }
+
+        private void btnBasla_Click(object sender, EventArgs e)
+        {
+            panel2.Visible=true;
+            lblSonuc.Text = "Tahmininizi Giriniz!";
+            
+            tahmin_sayisi = 10;
+            lblKalanHak.Text="kalan Hak:"+tahmin_sayisi;
+
+
+            txtTahmin.Enabled=true;
+            txtTahmin.Focus();
+            
+
+            txtTahmin.Clear();
+            listBox1.Items.Clear();
+            lblYazi.Text = "";
+           
+        }
+    }
+}
